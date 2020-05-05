ab310188eb835da20facadc626f8b2da03998c22
valkyrie-max <alisakitkina2016@gmail.com>
2020-05-05 16:51:51 -0500

    added primary components

--- src/App.js
+++ src/App.js
@@ -1,53 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
-import axios from 'axios'
+import axios from 'axios';
+import LandingPage from './Components/LandingPage';
+import RocketsInfo from './Components/RocketsInfo';
+import GameBoard from './Components/GameBoard';
+import GameBoard2 from './Components/GameBoard2';
 
 
 class App extends Component {
-  constructor(){
-    super();
-    this.state = {
-        allDataArray: [],
-        rocketArray: [],
-        missionArray: [],
-        shipArray: [],
-        launchArray: []
-    }
-  }
-  componentDidMount(){
-    // this.getRockets()
-    this.getApiData()
-  }
-
-    getApiData = () => {
-    let rockets =`https://api.spacexdata.com/v3/rockets`;
-    let missions = `https://api.spacexdata.com/v3/missions`;
-    let ships = `https://api.spacexdata.com/v3/ships`;
-    let launches = `https://api.spacexdata.com/v3/launches`;
-    const requestOne = axios.get(rockets);
-    const requestTwo = axios.get(missions);
-    const requestThree = axios.get(ships);
-    const requestFour = axios.get(launches);
-
-    axios.all([requestOne, requestTwo, requestThree, requestFour]).then(axios.spread((...responses) => {
-      const infoOne = responses[0];
-      const infoTwo = responses[1];
-      const infoThree = responses[2];
-      const infoFour = responses[3];
-      // console.log(infoOne, infoTwo, infoThree, infoFour, responses)
-      this.setState({
-        allDataArray: responses,
-        rocketArray: [infoOne],
-        missionArray: [infoTwo],
-        shipArray: [infoThree],
-        launchArray: [infoFour]
-      })
-      console.log(this.state.allDataArray, this.state.rocketArray, this.state.missionArray, this.state.shipArray, this.state.launchArray)
-    })).catch(errors => {
-      console.log(errors, 'it didnt work!')
-    })
-  }
-
   // getRockets = () => {
   //   const url = 'https://api.spacexdata.com/v3/rockets/'
   
@@ -65,9 +25,15 @@ class App extends Component {
 
   render() {
     return (
-      <main>
-        <h1>Hi guys!</h1>
-      </main>
+      <>
+        <LandingPage />
+        <main>
+          <h1>Hi guys!</h1>
+          <GameBoard />
+          <GameBoard2 />
+          <RocketsInfo />
+        </main>
+      </>
     )
   }
 }
