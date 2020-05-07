c84366acf443a5df726b37c646d38e6125c610f7
Lucas Silbernagel <lucas.silbernagel@gmail.com>
2020-05-06 22:45:30 -0400

    created rockets object and targeting function

--- src/Components/GameCells.js
+++ src/Components/GameCells.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Rockets from './Rockets'
 
 const cellsArray = [
     {
@@ -60,9 +61,9 @@ class GameCells extends Component {
                 hit: false,
                 empty: true
             }) 
-            console.log(e.currentTarget.id) 
+            console.log("You miss!") 
         } else {
-            console.log("Already hit");
+            console.log("Hit!");
         }
             
     }
@@ -74,72 +75,76 @@ class GameCells extends Component {
 
             
 
-                <div className="board">
+            <div className="board">
+                <Rockets />
+
                     <div className="messageArea"></div>
                     <table>
-                        <tr>
-                            <td onClick={this.clickFunction} id="1">A1</td>
-                            <td id="2">A2</td>
-                            <td id="3">A3</td>
-                            <td id="4">A4</td>
-                            <td id="5">A5</td>
-                            <td id="6">A6</td>
-                            <td id="7">A7</td>
-                    </tr>
-                    <tr>
-                            <td id="8">B1</td>
-                            <td id="9">B2</td>
-                            <td id="10">B3</td>
-                            <td id="11">B4</td>
-                            <td id="12">B5</td>
-                            <td id="13">B6</td>
-                            <td id="14">B7</td>
-                    </tr>
-                    <tr>
-                            <td id="15">C1</td>
-                            <td id="16">C2</td>
-                            <td id="17">C3</td>
-                            <td id="18">C4</td>
-                            <td id="19">C5</td>
-                            <td id="20">C6</td>
-                            <td id="21">C7</td>
-                    </tr>
-                    <tr>
-                            <td id="22">D1</td>
-                            <td id="23">D2</td>
-                            <td id="24">D3</td>
-                            <td id="25">D4</td>
-                            <td id="26">D5</td>
-                            <td id="27">D6</td>
-                            <td id="28">D7</td>
-                    </tr>
-                    <tr>
-                            <td id="29">E1</td>
-                            <td id="30">E2</td>
-                            <td id="31">E3</td>
-                            <td id="32">E4</td>
-                            <td id="33">E5</td>
-                            <td id="34">E6</td>
-                            <td id="35">E7</td>
-                    </tr>
-                    <tr>
-                            <td id="36">F1</td>
-                            <td id="37">F2</td>
-                            <td id="38">F3</td>
-                            <td id="39">F4</td>
-                            <td id="40">F5</td>
-                            <td id="41">F6</td>
-                            <td id="42">F7</td>
-                    </tr>
-                    <tr>
-                            <td id="43">G1</td>
-                            <td id="44">G2</td>
-                            <td id="45">G3</td>
-                            <td id="46">G4</td>
-                            <td id="47">G5</td>
-                            <td id="48">G6</td>
-                            <td id="49">G7</td>
-                        </tr>
+                        <tbody>
+                            <tr>
+                                <td onClick={this.clickFunction} id="1">A1</td>
+                                <td id="2">A2</td>
+                                <td id="3">A3</td>
+                                <td id="4">A4</td>
+                                <td id="5">A5</td>
+                                <td id="6">A6</td>
+                                <td id="7">A7</td>
+                            </tr>
+                            <tr>
+                                <td id="8">B1</td>
+                                <td id="9">B2</td>
+                                <td id="10">B3</td>
+                                <td id="11">B4</td>
+                                <td id="12">B5</td>
+                                <td id="13">B6</td>
+                                <td id="14">B7</td>
+                            </tr>
+                            <tr>
+                                <td id="15">C1</td>
+                                <td id="16">C2</td>
+                                <td id="17">C3</td>
+                                <td id="18">C4</td>
+                                <td id="19">C5</td>
+                                <td id="20">C6</td>
+                                <td id="21">C7</td>
+                            </tr>
+                            <tr>
+                                <td id="22">D1</td>
+                                <td id="23">D2</td>
+                                <td id="24">D3</td>
+                                <td id="25">D4</td>
+                                <td id="26">D5</td>
+                                <td id="27">D6</td>
+                                <td id="28">D7</td>
+                            </tr>
+                            <tr>
+                                <td id="29">E1</td>
+                                <td id="30">E2</td>
+                                <td id="31">E3</td>
+                                <td id="32">E4</td>
+                                <td id="33">E5</td>
+                                <td id="34">E6</td>
+                                <td id="35">E7</td>
+                            </tr>
+                            <tr>
+                                <td id="36">F1</td>
+                                <td id="37">F2</td>
+                                <td id="38">F3</td>
+                                <td id="39">F4</td>
+                                <td id="40">F5</td>
+                                <td id="41">F6</td>
+                                <td id="42">F7</td>
+                            </tr>
+                            <tr>
+                                <td id="43">G1</td>
+                                <td id="44">G2</td>
+                                <td id="45">G3</td>
+                                <td id="46">G4</td>
+                                <td id="47">G5</td>
+                                <td id="48">G6</td>
+                                <td id="49">G7</td>
+                            </tr>
+                        </tbody>
                     </table>
                     <form action="#">
                         <input id="userInput" type="text" placeholder="A1" />
@@ -148,9 +153,6 @@ class GameCells extends Component {
                 </div>
 
 
-            
-                
-          
         )
     }
 }
---
+++ src/Components/Rockets.js
@@ -0,0 +1,88 @@
+import React, { Component } from 'react';
+
+const template = {
+  boardSize: 7,
+  rockets: [
+    {
+      falcon1: {
+        numShips: 1,
+        shipLength: 1,
+        details: [
+          {
+            locations: [0],
+            hits: [""]
+          }
+        ]
+    } },
+    {
+      falcon9: {
+        numShips: 1,
+        shipLength: 2,
+        details: [
+          {
+            locations: [0, 0],
+            hits: ["", ""]
+          }
+        ]
+    } },
+    {
+      falconHeavy: {
+        numShips: 1,
+        shipLength: 3,
+        details: [
+          {
+            locations: [0, 0, 0],
+            hits: ["", "", ""]
+          }
+        ]
+    } },
+    {
+      starship: {
+        numShips: 1,
+        shipLength: 4,
+        details: [
+          {
+            locations: [0, 0, 0, 0],
+            hits: ["", "", "", ""]
+          }
+        ]
+    } }
+  ],
+  boom: function (boomSpot) {
+    for (let i = 0; i < template.rockets[i].numShips; i++) {
+      let rocket = this.rockets[i];
+      let index = rocket.location.indexOf(boomSpot);
+      if (rocket.hits[index] === "hit") {
+        view.displayMessage("You've hits this location");
+        return true;
+      } else if (index >= 0) {
+        rocket.hits[index] = "hit"
+        view.displayHit(boomSpot)
+        view.displayMessage("Hit")
+        if (this.isSunk(rocket)) {
+          view.displayMessage("You sunk my battleship")
+          this.shipsSunk++;
+        }
+        return true;
+      }
+    }
+    view.displayMiss(boomSpot);
+    view.displayMessage("You missed!");
+    return false
+  } 
+}
+
+class Rockets extends Component {
+  
+  render() {
+    console.log(template.boom);
+    template.boom()
+    return (
+      <div>
+        <p>Hi hello hi</p>
+      </div>
+  )
+}
+}
+
+export default Rockets;
