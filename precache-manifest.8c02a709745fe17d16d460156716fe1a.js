b22c612970faef860bf18bf8e7be3d572ee275fa
Lucas Silbernagel <lucas.silbernagel@gmail.com>
2020-05-08 15:57:32 -0400

    Updates

---
+++ precache-manifest.8c02a709745fe17d16d460156716fe1a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "00ca25cbafd22b4b54036b40c3bc8660",
+    "url": "/battleRockets/index.html"
+  },
+  {
+    "revision": "e6ca90fd875da8c18dc0",
+    "url": "/battleRockets/static/css/main.0fe07b6b.chunk.css"
+  },
+  {
+    "revision": "0933beda6ebf16277ae7",
+    "url": "/battleRockets/static/js/2.9d55e53f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/battleRockets/static/js/2.9d55e53f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e6ca90fd875da8c18dc0",
+    "url": "/battleRockets/static/js/main.f60e9065.chunk.js"
+  },
+  {
+    "revision": "0b5b3fbc5a206e55536f",
+    "url": "/battleRockets/static/js/runtime-main.046336e5.js"
+  }
+]);
